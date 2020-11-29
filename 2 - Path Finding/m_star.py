bb29f78ea481c9e74c83d673de41ca5a5b30bfe8
Matteo GLOCK <mglock@outlook.fr>
2020-11-29 23:15:50 +0100

    Travail sur la matrice temporelle , refonte de la trajectory en cours..

--- 2 - Path Finding/m_star.py
+++ 2 - Path Finding/m_star.py
@@ -37,13 +37,14 @@ class Horloge :
         self.time = time.time()
     
     def get_time(self,rounded=True) :
-        #Récupère le temps écoulé depuis le dernier set_time
+        """Récupère le temps écoulé depuis le dernier set_time"""
         if rounded :
             return round(time.time() - self.time, 3)
         else :
             return time.time() - self.time
 
 class Matrice_temporelle(Horloge) :
+    """Matrice contenant un couple de réel"""
 
     def __init__(self,taille=(0,0)):
         self.x,self.y = taille
@@ -51,19 +52,28 @@ class Matrice_temporelle(Horloge) :
         self.set_time()
     
     def send_trajectory(self,liste):
-        #fonction qui reçoit une liste de mouvement et qui l'ajoute à la liste
-        pass
+        """Fonction qui reçoit une liste de couple (case,temps de mouvement estimé pour se déplacer à la case) et qui l'ajoute à la matrice"""
+        temps = 0
+        tmp_bool = True  ##Ici le booléen temporaire permet d'actualiser la matrice seulement une fois pour get_case()
+        for (case,tm) in liste :
+            i,j = case
+            valcase = self.get_case(i,j,tmp_bool)
+            self.set_case(i,j,valcase + temps + tm)
+            temps += tm
+            tmp_bool = False
+
 
     def actualiser(self):
-        #Fonction qui retire le temps qui s'est écoulé à toute la matrice
+        """Fonction qui retire le temps qui s'est écoulé à toute la matrice"""
         delta = self.get_time()
         for i in range(self.x) :
             for j in range(self.y) :
                 case = self.matrice[i][j]
                 self.set_case(i,j,round(case - delta,3))
 
-    def get_case(self,a,b) :
-        self.actualiser()
+    def get_case(self,a,b,boole=True) :
+        if boole :
+            self.actualiser()
         return self.matrice[a][b]
 
     def get_matrice(self) :
@@ -71,6 +81,7 @@ class Matrice_temporelle(Horloge) :
         return self.matrice
 
     def set_case(self,a,b,x):
+        #Pas besoin d'actualiser ici
         if x >= 0.0 :
             self.matrice[a][b] = x
         else :
