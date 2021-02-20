5683868f1a36999f648d69e2f0a3debcdc7b5a11
Matteo GLOCK <mglock@outlook.fr>
2021-02-20 14:34:34 +0100

    nettoyage

--- database.py
+++ database.py
@@ -4,4 +4,4 @@ margin_error           = 15 #ms
 horizontally_move_time = 10 #ms
 rotation_move_time     = 10 #ms
 size                   = (20,20) #cases
-max_move = max(horizontally_move_time,rotation_move_time)
+max_move               = max(horizontally_move_time,rotation_move_time)
